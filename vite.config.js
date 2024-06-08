4925155da72bd88ca0330bff96102199af6c104b
Jubar Alam <jubaralam11022@gmail.com>
2024-06-08 20:52:03 +0530

    node module updated

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import commonjs from '@rollup/plugin-commonjs';
@@ -16,6 +15,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': '/src'
-    }
+    },
+    mainFields: ['module', 'main']
   }
 });
